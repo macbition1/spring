3081e1c9e43edd559fd642d76874edb2f6b0daa5
Chunbiao Huang s275935 <s275935@studenti.polito.it>
2022-11-15 17:39:49 +0100

    autowire for bean

--- spring_01_quickStart/src/main/java/com/chun/dao/impl/BookDaoImpl.java
+++ spring_01_quickStart/src/main/java/com/chun/dao/impl/BookDaoImpl.java
@@ -4,28 +4,12 @@ import com.chun.dao.BookDao;
 
 public class BookDaoImpl implements BookDao {
 
-    private int connectionNum;
-    private String dataBaseName;
 
-    public void setConnectionNum(int connectionNum) {
-        this.connectionNum = connectionNum;
-    }
-
-    public void setDataBaseName(String dataBaseName) {
-        this.dataBaseName = dataBaseName;
-    }
 
     public void save(){
-        System.out.println("Book dao save..." + connectionNum +"," + dataBaseName);
+        System.out.println("Book dao save...");
     }
 
-    public void init(){
-        System.out.println("init method...");
-    }
-
-    public void destroy(){
-        System.out.println("destroy method...");
-    }
 
 
 
--- spring_01_quickStart/src/main/java/com/chun/service/impl/BookServiceImpl.java
+++ spring_01_quickStart/src/main/java/com/chun/service/impl/BookServiceImpl.java
@@ -8,16 +8,14 @@ public class  BookServiceImpl implements BookService {
 //    private BookDao bookDao = new BookDaoImpl();
     private BookDao bookDao;
 
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
     public void save(){
         System.out.println("Book service save....");
         bookDao.save();
     }
 
-    public BookServiceImpl(BookDao bookDao) {
-        this.bookDao = bookDao;
-    }
-//Provide related setter method, and then in applicationContext.xml set the relationship
-//    public void setBookDao(BookDao bookDao) {
-//        this.bookDao = bookDao;
-//    }
+
 }
