ad21939555b61b8e0c3ad0789422312a794689ce
Chunbiao Huang s275935 <s275935@studenti.polito.it>
2022-11-15 18:15:32 +0100

    connect dataSource

--- spring_01_quickStart/src/main/java/com/chun/App.java
+++ spring_01_quickStart/src/main/java/com/chun/App.java
@@ -5,14 +5,20 @@ import com.chun.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.sql.DataSource;
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
 
         //Get IoC container
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        BookService bookService = (BookService) ctx.getBean("bookService");
-        bookService.save();
+//        BookService bookService = (BookService) ctx.getBean("bookService");
+//        bookService.save();
+
+        DataSource dataSource = (DataSource) ctx.getBean("dataSource");
+        System.out.println(dataSource);
 
     }
 }
